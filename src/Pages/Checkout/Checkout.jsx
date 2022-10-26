1d256416f5af4e2a0b97cea59195db5722d7cac2
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-10-26 15:12:56 +0600

    added checkout pages

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,8 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+
 const Checkout = () => {
+  const course = useLoaderData();
+  const {name, price} = course;
 
   return (
-    <div>
-      
+    <div className="card w-96 bg-base-100 shadow-xl mx-auto">
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>Price: ${price}</p>
+        <div className="card-actions">
+          <button className="btn btn-primary w-full">Get access</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/contexts/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import {getAuth} from 'firebase/auth';
+
+const AuthProvider = () => {
+  const auth = getAuth()
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default AuthProvider;
